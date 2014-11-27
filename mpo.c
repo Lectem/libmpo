08b09c1b2b36e996c0a49d02331f1a0c9f489f2a
lectem <lectem@gmail.com>
2014-11-27 15:19:48 +0100

    MP Index IFD done up to MPEntry

--- mpo.c
+++ mpo.c
@@ -70,19 +70,58 @@ jpeg_getint32 (j_decompress_ptr cinfo, int swapEndian)
 
 /* See the DC-007_E Specification. */
 /* 5.2.2.1  Table 3, page 13 */
-typedef enum{ LITTLE_ENDIAN = 0x49492A,
+typedef enum{ LITTLE_ENDIAN = 0x49492A00,
     BIG_ENDIAN = 0x4D4D002A }MPExt_ByteOrder;
 
 /* See the DC-007_E Specification. */
 /* 5.2.2.3  Table 3, page 13 */
 typedef enum
 {
+    /*Mandatory*/
     MPTag_MPFVersion        = 0xB000,
     MPTag_NumberOfImages    = 0xB001,
     MPTag_MPEntry           = 0xB002,
+    /*Optional*/
     MPTag_ImageUIDList      = 0xB003,
     MPTag_TotalFrames       = 0xB004
 }MPExt_MPIndexIFDTags;
+
+typedef union
+{
+    long value;
+    struct {
+        unsigned int dependentParent:1;
+        unsigned int dependentChild:1;
+        unsigned int representativeImage:1;
+        unsigned int reserved:2;
+        unsigned int imgType:3;
+        unsigned int MPTypeCode:24;
+    } data;
+}
+MPExt_IndividualImageAttr;
+
+typedef enum
+{
+    MPType_Baseline =0x030000,
+    MPType_LargeThumbnail_Mask =0x010000,
+    MPType_LargeThumbnail_Class1 =0x010001, /*VGA Equivalent*/
+    MPType_LargeThumbnail_Class2 =0x010002, /*Full HD Equivalent*/
+    MPType_MultiFrame_Mask =0x030000,
+    MPType_MultiFrame_Panorama =0x030001,
+    MPType_MultiFrame_Disparity =0x030002,
+    MPType_MultiFrame_MultiAngle =0x030003
+}
+MPExt_MPType;
+
+typedef struct
+{
+    MPExt_IndividualImageAttr individualImgAttr;
+    long size;
+    long offset;
+    INT16 dependentImageEntry1;
+    INT16 dependentImageEntry2;
+}
+MPExt_MPEntry;
 
 typedef struct
 {
@@ -91,7 +130,8 @@ typedef struct
     INT32 first_IFD_offset;
     /*MP Index IFD*/
     INT16 count;
-    char version[12];
+    char version[4];
+    long numberOfImages;
 
 }
 MPExt_Data;
@@ -107,27 +147,69 @@ print_APP02_MPF (MPExt_Data *data)
         perror("Not an MP extended file.");
         return 0;
     }
-    printf("\nMPF version :%12s\n",data->version);
+    printf("\n\n-------------MPF extension data-------------\n");
+    printf("MPF version:\t\t%.4s\n",data->version);
     if(data->byte_order == LITTLE_ENDIAN)
-        printf("Byte order : little endian\n");
+        printf("Byte order:\t\tlittle endian\n");
     else if(data->byte_order == BIG_ENDIAN)
-        printf("Byte order : big endian\n");
+        printf("Byte order:\tbig endian\n");
     else printf("Couldn't recognize byte order : 0x%x\n",data->byte_order);
-    printf("First FID offset : 0x%x\n",data->first_IFD_offset);
-    printf("\n");
+    printf("First FID offset:\t0x%x\n",(unsigned int)data->first_IFD_offset);
+    printf("---MP Index IFD---\n");
+    printf("Count:\t\t\t%d(0x%x)\n",data->count,data->count);
+    printf("Number of images:\t%ld\n",data->numberOfImages);
+    printf("-----------------End of MPF-----------------\n\n\n");
+    return TRUE;
 }
 
 
 
+METHODDEF(void)
+MPExtReadTag (j_decompress_ptr cinfo,MPExt_Data *data,int* length, int swapEndian)
+{
+    int i;
+    unsigned int tag=0;
+    tag=jpeg_getint16(cinfo,swapEndian);
+    *length-=2;
+    switch(tag)
+    {
+    case MPTag_MPFVersion :
+        /*Specification says that the version count = 4 but that the total length of TAG+DATA is 12 */
+        /*Retrieve only the 4 lasts characters, should be equal to 0100                             */
+        for(i=0;i<6;++i)(void)jpeg_getc(cinfo);//Discard the 6 first bytes : not used by the specs
+        for(i=0;i<4;++i)data->version[i]=jpeg_getc(cinfo);
+        break;
+    case MPTag_NumberOfImages :
+        /*NumberOfImages block size is 12bytes*/
+        jpeg_getint16(cinfo,swapEndian);*length-=2;
+        jpeg_getint32(cinfo,swapEndian);*length-=4;
+        /*Long value = last for 4 bytes ?*/
+        data->numberOfImages=jpeg_getint32(cinfo,swapEndian);*length-=4;
+        break;
+    case MPTag_MPEntry:
+
+        break;
+    /*Non mandatory*/
+    default:
+        printf("-----------------------TAG : 0x%x--------------------\n",tag);
+        break;
+    }
+}
 METHODDEF(boolean)
 MPExtReadAPP02 (j_decompress_ptr cinfo)
 {
     int i=0;
-    MPExt_Data data;
+    MPExt_Data data=
+    {
+        .MPF_identifier={0},
+        .byte_order=LITTLE_ENDIAN,
+        .version={0},
+    };
     int length;
     unsigned int ch=0;
     length = jpeg_getc(cinfo) << 8;
     length += jpeg_getc(cinfo);
+    printf( "APP02, length %d:\n",length);
     length -= 2;
     for(i=0;i<4;++i)data.MPF_identifier[i]=jpeg_getc(cinfo);
     length-=4;
@@ -136,9 +218,9 @@ MPExtReadAPP02 (j_decompress_ptr cinfo)
        data.MPF_identifier[2] != 'F'||
        data.MPF_identifier[3] != 0)
     {
-        perror("Not an MP extended file.\n");
+        /*Ignore this block*/
         while(length-- >0){jpeg_getc(cinfo);}
-        return 0;
+        return 1;
     }
     data.byte_order= jpeg_getint32(cinfo,1);
     length-=4;
@@ -149,16 +231,108 @@ MPExtReadAPP02 (j_decompress_ptr cinfo)
     length-=4;
     data.count=jpeg_getint16(cinfo,endiannessSwap);
     length-=2;
-/*TODO move TAG|Value parsing into another func*/
-/*
-    for(i=0;i<12;++i)
-        data.version[i]=jpeg_getc(cinfo);
-    length-=12;
-*/
-
+    MPExtReadTag(cinfo,&data,&length,endiannessSwap);
+    MPExtReadTag(cinfo,&data,&length,endiannessSwap);
     print_APP02_MPF(&data);
     return 1;
 }
+
+
+
+
+METHODDEF(boolean)
+print_text_marker (j_decompress_ptr cinfo);
+
+
+
+void decompress_mpo(char* filename)
+{
+    FILE* in;
+    in=fopen(filename,"rb");
+    if(in)
+    {
+        printf("%s file opened.\n", filename);
+        fseek(in,0,SEEK_END);
+        long size = ftell(in);
+        rewind(in);
+        unsigned char * src_buffer=calloc(size,sizeof(*src_buffer));
+        if(src_buffer)
+        {
+            fread(src_buffer,size,sizeof(*src_buffer),in);
+            decompress_mpo_from_mem(src_buffer,size);
+
+            int index=0;
+            int i;
+            /*Quick and dirty hack to reach 2nd image*/
+            for(i=1; i<size-3; i++)
+            {
+                if(src_buffer[i] ==0xFF && src_buffer[i+1] ==0xD8
+                        && src_buffer[i+2] ==0xFF && src_buffer[i+3] ==0xE1)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if(index)
+            {
+                printf("Second SOI offset : %d(0x%x)\n",index,index);
+                decompress_mpo_from_mem(src_buffer+index,size-index);
+            }
+            else printf("couldn't find second SOI marker.\n");
+
+            free(src_buffer);
+        }
+        fclose(in);
+    }
+
+}
+
+
+
+void decompress_mpo_from_mem(unsigned char* src_buffer,long size)
+{
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    JSAMPARRAY buffer; /*output*/
+
+    int row_stride;		/* physical row width in output buffer */
+
+
+
+    cinfo.err = jpeg_std_error(&jerr);
+    printf("Creating decompress object\n");
+
+    jpeg_create_decompress(&cinfo);
+    int i=3;
+    jpeg_set_marker_processor(&cinfo, JPEG_APP0+2, MPExtReadAPP02);
+
+    jpeg_mem_src(&cinfo, src_buffer,size);
+    printf("Reading header...\n");
+    jpeg_read_header(&cinfo, TRUE);
+    printf("Size of Jpeg buffer: %d(0x%x)\n",((struct jpeg_source_mgr*)cinfo.src)->bytes_in_buffer,((struct jpeg_source_mgr*)cinfo.src)->bytes_in_buffer);
+    printf("Starting decompression...\n");
+
+    jpeg_start_decompress(&cinfo);
+    printf("Image is of size %dx%d\n",cinfo.output_width,cinfo.output_height);
+    //The size of a line
+    row_stride = cinfo.output_width * cinfo.output_components;
+    /* Make a one-row-high sample array that will go away when done with image */
+    buffer = (*cinfo.mem->alloc_sarray)
+             ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+
+    while (cinfo.output_scanline < cinfo.output_height)
+    {
+        //printf("Reading line %d\n",cinfo.output_scanline);
+        jpeg_read_scanlines(&cinfo, buffer, 1);
+
+    }
+    printf("Finishing decompression...\n\n\n");
+    jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
+}
+
+
 
 
 
@@ -224,101 +398,5 @@ print_text_marker (j_decompress_ptr cinfo)
 
     return TRUE;
 }
-
-
-
-
-
-
-void decompress_mpo(char* filename)
-{
-    FILE* in;
-    in=fopen(filename,"rb");
-    if(in)
-    {
-        printf("%s file opened.\n", filename);
-        fseek(in,0,SEEK_END);
-        long size = ftell(in);
-        rewind(in);
-        unsigned char * src_buffer=calloc(size,sizeof(*src_buffer));
-        if(src_buffer)
-        {
-            fread(src_buffer,size,sizeof(*src_buffer),in);
-            decompress_mpo_from_mem(src_buffer,size);
-
-            int index=0;
-            int i;
-            /*Quick and dirty hack to reach 2nd image*/
-            for(i=1; i<size-3; i++)
-            {
-                if(src_buffer[i] ==0xFF && src_buffer[i+1] ==0xD8
-                        && src_buffer[i+2] ==0xFF && src_buffer[i+3] ==0xE1)
-                {
-                    index = i;
-                    break;
-                }
-            }
-            if(index)
-            {
-                printf("Second SOI offset : %d(0x%x)\n",index,index);
-                decompress_mpo_from_mem(src_buffer+index,size-index);
-            }
-            else printf("couldn't find second SOI marker.\n");
-
-            free(src_buffer);
-        }
-        fclose(in);
-    }
-
-}
-
-
-
-void decompress_mpo_from_mem(unsigned char* src_buffer,long size)
-{
-    struct jpeg_decompress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-
-    JSAMPARRAY buffer; /*output*/
-
-    int row_stride;		/* physical row width in output buffer */
-
-
-
-    cinfo.err = jpeg_std_error(&jerr);
-    printf("Creating decompress object\n");
-
-    jpeg_create_decompress(&cinfo);
-    int i=3;
-    for(; i<13; ++i)
-        jpeg_set_marker_processor(&cinfo, JPEG_APP0+i, print_text_marker);
-    jpeg_set_marker_processor(&cinfo, JPEG_APP0+2, MPExtReadAPP02);
-
-    jpeg_mem_src(&cinfo, src_buffer,size);
-    printf("Reading header...\n");
-    jpeg_read_header(&cinfo, TRUE);
-    printf("Size of Jpeg buffer: %d(0x%x)\n",((struct jpeg_source_mgr*)cinfo.src)->bytes_in_buffer,((struct jpeg_source_mgr*)cinfo.src)->bytes_in_buffer);
-    printf("Starting decompression...\n");
-
-    jpeg_start_decompress(&cinfo);
-    printf("Image is of size %dx%d\n",cinfo.output_width,cinfo.output_height);
-    //The size of a line
-    row_stride = cinfo.output_width * cinfo.output_components;
-    /* Make a one-row-high sample array that will go away when done with image */
-    buffer = (*cinfo.mem->alloc_sarray)
-             ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
-
-    while (cinfo.output_scanline < cinfo.output_height)
-    {
-        //printf("Reading line %d\n",cinfo.output_scanline);
-        jpeg_read_scanlines(&cinfo, buffer, 1);
-
-    }
-//    printf("offset%d\n",cinfo.src.next_input_byte()-in);
-    jpeg_finish_decompress(&cinfo);
-    jpeg_destroy_decompress(&cinfo);
-}
-
-
 
 
