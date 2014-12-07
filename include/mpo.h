411d2c9a92f05b1ee7a31708bcb78cdf829836e1
lectem <lectem@gmail.com>
2014-12-07 02:25:16 +0100

    Fixed MPExt_IndividualImageAttr bits order

--- include/mpo.h
+++ include/mpo.h
@@ -10,7 +10,12 @@
  */
 
 
-
+typedef enum
+{
+    MP_LONG = 4,
+    MP_UNDEFINED = 7
+}
+MPVal_types;
 
 /* See the DC-007_E Specification. */
 /* 5.2.2.1  Table 3, page 13 */
@@ -50,28 +55,28 @@ typedef enum
 
 typedef union
 {
-    long value;
+    INT32 value;
     struct {
-        unsigned int dependentParent:1;
-        unsigned int dependentChild:1;
-        unsigned int representativeImage:1;
-        unsigned int reserved:2;
-        unsigned int imgType:3;
         unsigned int MPTypeCode:24;
+        unsigned int imgType:3;
+        unsigned int reserved:2;
+        unsigned int representativeImage:1;
+        unsigned int dependentChild:1;
+        unsigned int dependentParent:1;
     } data;
 }
 MPExt_IndividualImageAttr;
 
 typedef enum
 {
-    MPType_LargeThumbnail_Mask  =0x010000,
-    MPType_LargeThumbnail_Class1=0x010001, /*VGA Equivalent*/
-    MPType_LargeThumbnail_Class2=0x010002, /*Full HD Equivalent*/
-    MPType_MultiFrame_Mask      =0x020000,
-    MPType_MultiFrame_Panorama  =0x020001,
-    MPType_MultiFrame_Disparity =0x020002,
-    MPType_MultiFrame_MultiAngle=0x020003,
-    MPType_Baseline             =0x030000
+    MPType_LargeThumbnail_Mask  =0x10000,
+    MPType_LargeThumbnail_Class1=0x10001, /*VGA Equivalent*/
+    MPType_LargeThumbnail_Class2=0x10002, /*Full HD Equivalent*/
+    MPType_MultiFrame_Mask      =0x20000,
+    MPType_MultiFrame_Panorama  =0x20001,
+    MPType_MultiFrame_Disparity =0x20002,
+    MPType_MultiFrame_MultiAngle=0x20003,
+    MPType_Baseline             =0x30000
 }
 MPExt_MPType;
 
@@ -115,7 +120,7 @@ MPExt_Data;
 typedef struct
 {
     MPExt_Data APP02;
-    struct jpeg_compress_struct cinfo;
+    struct jpeg_compress_struct *cinfo;
     JOCTET ** images_data;
 }
 mpo_compress_struct;
@@ -124,16 +129,19 @@ mpo_compress_struct;
 typedef struct
 {
     MPExt_Data APP02;
-    struct jpeg_decompress_struct cinfo;
+    struct jpeg_decompress_struct *cinfo;
     JOCTET ** images_data;
 }
 mpo_decompress_struct;
 
 
 void decompress_mpo(char* filename);
-void decompress_mpo_from_mem(unsigned char* src,long size);
+void decompress_mpo_from_mem(unsigned char* src,long size,int isFirstImage);
+boolean MPExtReadAPP02AsFirstImage(j_decompress_ptr cinfo);
 boolean MPExtReadAPP02 (j_decompress_ptr cinfo);
 
 
+
+inline char isLittleEndian();
 
 #endif // MPO_H_INCLUDED
