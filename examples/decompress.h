043777ebf9beb0391e7d7161753e9aed9a0b145b
Lectem <lectem@gmail.com>
2015-03-14 22:34:47 +0100

    cleanup gh pages

--- examples/decompress.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Lectem on 12/03/2015.
-//
-
-#ifndef _LIBMPO_SOURCE_FILE_H_
-#define _LIBMPO_SOURCE_FILE_H_
-
-#endif //_LIBMPO_SOURCE_FILE_H_
--- exportdoc.sh
+++
@@ -1,5 +0,0 @@
-doxygen Doxyfile
-cd html
-git add --all
-git commit -m"Doc generated from Travis build #$TRAVIS_BUILD_NUMBER"
-git push -fq origin gh-pages > /dev/null
