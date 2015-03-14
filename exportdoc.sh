6b8402ea4fe71159880b00ef115b729aba13e34f
Lectem <lectem@gmail.com>
2015-03-14 22:46:35 +0100

    try again

--- exportdoc.sh
+++ exportdoc.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+if [ "${CC}" == gcc ]; then
+
 doxygen Doxyfile
 cd html
 git add --all
 git commit -m"Doc generated from Travis build #$TRAVIS_BUILD_NUMBER"
 git push -fq origin gh-pages > /dev/null
+
+fi
