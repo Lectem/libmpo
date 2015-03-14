eb99caefbdd7c4f124535d38cf3b43b223f46029
Lectem <lectem@gmail.com>
2015-03-14 23:11:23 +0100

    one more time~

--- exportdoc.sh
+++ exportdoc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-if [ "$CC" = "gcc" ]; then
+if [ "$CC" = "gcc" ] && [ "$TRAVIS_REPO_SLUG" == "Lectem/libmpo" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 doxygen Doxyfile
 cd html
 git add --all
 git commit -m"Doc generated from Travis build #$TRAVIS_BUILD_NUMBER"
-git push -fq origin gh-pages > /dev/null
+git push -f origin gh-pages
 
 fi
